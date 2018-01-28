b00dd4942eaec56a978ad2d53cf516c932ac9745
Kevin Huang <yellow.khaos@gmail.com>
2018-01-28 02:50:46 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -24,3 +24,29 @@ INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("End
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Bean", "I'm just an advisor", "human", 2,2);
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Peter", "I'm kinda crazy", "human", 2,2);
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Valentine", "Ithink ender is great", "human", 2,2);
+
+INSERT INTO character_books (character_id, book_id) VALUES (1,1);
+INSERT INTO character_books (character_id, book_id) VALUES (2,1);
+INSERT INTO character_books (character_id, book_id) VALUES (3,1);
+INSERT INTO character_books (character_id, book_id) VALUES (4,1);
+INSERT INTO character_books (character_id, book_id) VALUES (1,2);
+INSERT INTO character_books (character_id, book_id) VALUES (2,2);
+INSERT INTO character_books (character_id, book_id) VALUES (3,2);
+INSERT INTO character_books (character_id, book_id) VALUES (4,2);
+INSERT INTO character_books (character_id, book_id) VALUES (1,3);
+INSERT INTO character_books (character_id, book_id) VALUES (2,3);
+INSERT INTO character_books (character_id, book_id) VALUES (3,3);
+INSERT INTO character_books (character_id, book_id) VALUES (4,3);
+
+INSERT INTO character_books (character_id, book_id) VALUES (5,4);
+INSERT INTO character_books (character_id, book_id) VALUES (6,4);
+INSERT INTO character_books (character_id, book_id) VALUES (7,4);
+INSERT INTO character_books (character_id, book_id) VALUES (8,4);
+INSERT INTO character_books (character_id, book_id) VALUES (5,5);
+INSERT INTO character_books (character_id, book_id) VALUES (6,5);
+INSERT INTO character_books (character_id, book_id) VALUES (7,5);
+INSERT INTO character_books (character_id, book_id) VALUES (8,5);
+INSERT INTO character_books (character_id, book_id) VALUES (5,6);
+INSERT INTO character_books (character_id, book_id) VALUES (6,6);
+INSERT INTO character_books (character_id, book_id) VALUES (7,6);
+INSERT INTO character_books (character_id, book_id) VALUES (8,6);
